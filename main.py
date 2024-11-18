e3c971b6d0b733d9ddbc51aeeb8748072822c258
Crystal Chiu <chiubacca@Crystals-Air.lan>
2024-11-18 01:56:25 -0800

    setup

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ sys.path.append('gym-fightingice')
 from gym_fightingice.envs.Machete import Machete
 
 def main():
+    # we will need to define and train player 1
+
+    # set opponent
     env = gym.make("FightingiceDataNoFrameskip-v0", java_env_path="", port=4242)
     env.reset(p2=Machete)
     
     for i in range(300):
         env.step(0)
-
-
 
 
 if __name__ == "__main__":
